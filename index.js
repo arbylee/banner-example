484861421b551586915bf369fe2c33683811e435
Richard Lee <rblee88@gmail.com>
2018-05-07 17:21:29 -0500

    Start encapsulating logic

--- index.js
+++ index.js
@@ -1,85 +1,106 @@
 (function() {
-  const DEFAULT = 'default';
-  const FULL_WIDTH = 'full-width';
-  const EXPANDED = 'expanded';
-
   const HOVER = 'hover';
   const UNHOVER = 'unhover';
   const CLICK = 'click';
 
-  let currentState = DEFAULT;
+  const BannerImage = () => {
+    const DEFAULT = 'default';
+    const FULL_WIDTH = 'full-width';
+    const EXPANDED = 'expanded';
 
-  const defaultToExpanded = ($containerElement, $textElement) => {
-    $containerElement.addClass("grow");
-    $textElement.addClass('visible');
-    currentState = EXPANDED;
-  }
-  const expandedToDefault = ($containerElement, $textElement) => {
-    $containerElement.removeClass("grow");
-    $textElement.removeClass('visible');
-    currentState = DEFAULT;
-  }
-  const expandedToFullWidth = ($containerElement, $textElement) => {
-    $containerElement.removeClass("grow");
-    $containerElement.addClass('full-width');
-    currentState = FULL_WIDTH;
-  }
-  const fullWidthToDefault = ($containerElement, $textElement) => {
-    $containerElement.removeClass("full-width")
-    $textElement.removeClass('visible');
-    currentState = DEFAULT;
-  }
+    let currentState = DEFAULT;
 
-  const stateTransitions = {
-    [DEFAULT]: {
-      [HOVER]: defaultToExpanded,
-    },
-    [EXPANDED]: {
-      [UNHOVER]: expandedToDefault,
-      [CLICK]: expandedToFullWidth
-    },
-    [FULL_WIDTH]: {
-      [CLICK]: fullWidthToDefault
+    const defaultToExpanded = ($containerElement, $textElement) => {
+      $containerElement.addClass("grow");
+      $textElement.addClass('visible');
+      currentState = EXPANDED;
     }
-  };
-
-
-  const getStateHandler = (from, to) => {
-    const handler = stateTransitions[from] && stateTransitions[from][to];
-    return handler;
-  };
-
-  $('.image-container').mouseenter(function(e) {
-    const handler = getStateHandler(currentState, HOVER);
-    if (!handler) {
-      console.log(`No handler for transitioning ${currentState} with ${HOVER}`)
-      return;
+    const expandedToDefault = ($containerElement, $textElement) => {
+      $containerElement.removeClass("grow");
+      $textElement.removeClass('visible');
+      currentState = DEFAULT;
+    }
+    const expandedToFullWidth = ($containerElement, $textElement) => {
+      $containerElement.removeClass("grow");
+      $containerElement.addClass('full-width');
+      currentState = FULL_WIDTH;
+    }
+    const fullWidthToDefault = ($containerElement, $textElement) => {
+      $containerElement.removeClass("full-width")
+      $textElement.removeClass('visible');
+      currentState = DEFAULT;
     }
 
-    const side = $(this).data('side');
-    const $textOverlay = $(this).children(`.text-overlay[data-side="${side}"]`);
-    handler($(this), $textOverlay);
-  });
+    const stateTransitions = {
+      [DEFAULT]: {
+        [HOVER]: defaultToExpanded,
+      },
+      [EXPANDED]: {
+        [UNHOVER]: expandedToDefault,
+        [CLICK]: expandedToFullWidth
+      },
+      [FULL_WIDTH]: {
+        [CLICK]: fullWidthToDefault
+      }
+    };
 
-  $('.image-container').mouseleave(function(e) {
-    const handler = getStateHandler(currentState, UNHOVER);
-    if (!handler) {
-      return;
+    const getStateHandler = (action) => {
+      const handler = stateTransitions[currentState] && stateTransitions[currentState][action];
+      return handler;
+    };
+
+    const getTextSelector = (side) => {
+      return `.text-overlay[data-side="${side}"]`;
+    };
+
+    const getCurrentState = () => {
+      return currentState;
+    };
+
+    return {
+      getStateHandler,
+      containerSelector: '.image-container',
+      getTextSelector,
+      getCurrentState
     }
+  }
 
-    const side = $(this).data('side');
-    const $textOverlay = $(this).children(`.text-overlay[data-side="${side}"]`);
-    handler($(this), $textOverlay);
-  });
+  const init = () => {
+    const bannerImage = BannerImage();
 
-  $('.image-container').click(function(e) {
-    const handler = getStateHandler(currentState, CLICK);
-    if (!handler) {
-      return;
-    }
+    $(bannerImage.containerSelector).mouseenter(function(e) {
+      const handler = bannerImage.getStateHandler(HOVER);
+      if (!handler) {
+        return;
+      }
 
-    const side = $(this).data('side');
-    const $textOverlay = $(this).children(`.text-overlay[data-side="${side}"]`);
-    handler($(this), $textOverlay);
-  });
+      const side = $(this).data('side');
+      const $textOverlay = $(this).children(bannerImage.getTextSelector(side));
+      handler($(this), $textOverlay);
+    });
+
+    $(bannerImage.containerSelector).mouseleave(function(e) {
+      const handler = bannerImage.getStateHandler(UNHOVER);
+      if (!handler) {
+        return;
+      }
+
+      const side = $(this).data('side');
+      const $textOverlay = $(this).children(bannerImage.getTextSelector(side));
+      handler($(this), $textOverlay);
+    });
+
+    $(bannerImage.containerSelector).click(function(e) {
+      const handler = bannerImage.getStateHandler(CLICK);
+      if (!handler) {
+        return;
+      }
+
+      const side = $(this).data('side');
+      const $textOverlay = $(this).children(bannerImage.getTextSelector(side));
+      handler($(this), $textOverlay);
+    });
+  }
+
+  init();
 })();
