7d38acab9d9359c26b75f6fe8314de3c5901ce48
Richard Lee <rblee88@gmail.com>
2018-05-07 17:21:26 -0500

    Display some text on hover. Hovering text removes hover behavior. Transitioning between hovering sides seems choppy

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 (function() {
   $('.image-container').hover(function(e) {
     $(this).toggleClass('grow');
+    var side = $(this).data('side');
+    $('.text-overlay[data-side="' + side + '"]').toggleClass('visible');
   });
 })();
