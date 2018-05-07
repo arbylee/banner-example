4de6e836aa46fe5f9df6a76ad465bca5bbdb7863
Richard Lee <rblee88@gmail.com>
2018-05-07 17:21:24 -0500

    Hover working with javascript

---
+++ index.js
@@ -0,0 +1,9 @@
+(function() {
+  $('.left-image-container').hover(function(e) {
+    $(this).toggleClass('grow');
+  });
+
+  $('.right-image-container').hover(function(e) {
+    $(this).toggleClass('grow');
+  });
+})();
